bacdcc80ea34b944d93a586fe3d6ec79c694819a
minwoo <minwoo9621@gmail.com>
2023-11-27 22:05:16 +0900

    Install @nestjs/apollo and Add driver in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { typeOrmConfig } from './config/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     GraphQLModule.forRoot({
+      driver: ApolloDriver,
       autoSchemaFile: true,
     }),
   ],
