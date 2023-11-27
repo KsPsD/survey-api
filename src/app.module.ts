abeb03cda1762c1f11d213bc15f72880b49b0a6c
minwoo <minwoo9621@gmail.com>
2023-11-27 22:11:10 +0900

    Add app.resolver for testing graphql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,16 @@ import { typeOrmConfig } from './config/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
   ],
-  controllers: [],
-  providers: [],
+  providers: [AppResolver],
 })
 export class AppModule {}
