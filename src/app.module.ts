c1752d92de4b6ec733dac465b5cf92e1dee5c8be
minwoo <minwoo9621@gmail.com>
2023-11-27 21:35:54 +0900

    Remove app.service and app.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { typeOrmConfig } from './config/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -11,7 +9,7 @@ import { GraphQLModule } from '@nestjs/graphql';
       autoSchemaFile: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
