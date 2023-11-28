93a5423f0783234ef6d958b2461ff56167fcbceb
minwoo <minwoo9621@gmail.com>
2023-11-28 16:13:05 +0900

    Add objectType in survey entity

--- src/survey/survey.entity.ts
+++ src/survey/survey.entity.ts
@@ -1,21 +1,28 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, In } from 'typeorm';
 import { Question } from '../question/question.entity';
 import { BaseEntity } from '../base/base.entity';
+import { ObjectType, Field, Int, InputType } from '@nestjs/graphql';
 
+@ObjectType()
 @Entity()
 export class Survey extends BaseEntity {
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   title: string;
 
+  @Field({ nullable: true })
   @Column({ nullable: true })
   description: string;
 
+  @Field(() => [Question], { nullable: true })
   @OneToMany(() => Question, (question) => question.survey)
   questions: Question[];
 
+  @Field()
   @Column({ default: false })
   isCompleted: boolean;
 }
