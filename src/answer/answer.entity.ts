35f04a6ec598347ec3f862dc978fe671c505caf5
minwoo <minwoo9621@gmail.com>
2023-11-28 17:17:25 +0900

    Add Field decorator in other entity

--- src/answer/answer.entity.ts
+++ src/answer/answer.entity.ts
@@ -2,15 +2,19 @@ import { Entity, PrimaryGeneratedColumn, ManyToOne, Column } from 'typeorm';
 import { Question } from '../question/question.entity';
 import { Option } from '../option/option.entity';
 import { BaseEntity } from '../base/base.entity';
-
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+@ObjectType()
 @Entity()
 export class Answer extends BaseEntity {
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(() => Question)
   @ManyToOne(() => Question, (question) => question.answers)
   question: Question;
 
+  @Field(() => Option)
   @ManyToOne(() => Option, (option) => option.answers)
   selectedOption: Option;
 }
