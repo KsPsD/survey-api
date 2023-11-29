12b937c0fcb8b4abc2349fc01f2f41ea8f94d217
minwoo <minwoo9621@gmail.com>
2023-11-29 14:40:55 +0900

    Modify question input for adding surveyId in CreateQuestionInput

--- src/question/dto/question.input.ts
+++ src/question/dto/question.input.ts
@@ -1,5 +1,5 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { IsOptional, IsString } from 'class-validator';
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 @InputType()
 class QuestionInputBase {
@@ -9,10 +9,15 @@ class QuestionInputBase {
   content?: string;
 }
 @InputType()
-export class CreateQuestionInput extends QuestionInputBase {
+export class CreateQuestionInput {
   @Field()
   @IsString()
+  @IsNotEmpty()
   content: string;
+
+  @Field(() => Int)
+  @IsInt()
+  surveyId: number;
 }
 @InputType()
 export class UpdateQuestionInput extends QuestionInputBase {}
