9c1f71487fab0f0af465ff73106c2f7b5d8ed703
minwoo <minwoo9621@gmail.com>
2023-11-30 12:38:54 +0900

    Add complete survey service

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -1,12 +1,20 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Repository, EntityManager, DataSource, In } from 'typeorm';
 import { Survey } from './survey.entity';
-import { CreateSurveyInput, UpdateSurveyInput } from './dto/survey.input';
-
+import {
+  CompleteSurveyInput,
+  CreateSurveyInput,
+  UpdateSurveyInput,
+} from './dto/survey.input';
+import { Answer } from '../answer/answer.entity';
+import { Question } from '../question/question.entity';
+import { Option } from '../option/option.entity';
 @Injectable()
 export class SurveyService {
+  private readonly logger = new Logger(SurveyService.name);
   constructor(
+    @Inject('DATA_SOURCE')
+    private dataSource: DataSource,
     @Inject('SURVEY_REPOSITORY')
     private surveyRepository: Repository<Survey>,
   ) {}
@@ -48,5 +56,52 @@ export class SurveyService {
       throw new NotFoundException(`Survey with ID ${id} not found`);
     }
     return true;
+  }
+
+  async completeSurvey(
+    id: number,
+    completeSurveyInput: CompleteSurveyInput,
+  ): Promise<Boolean> {
+    let isSuccess = false;
+    await this.dataSource.transaction(async (manager: EntityManager) => {
+      const { answers } = completeSurveyInput;
+      const survey = await manager.findOne(Survey, { where: { id } });
+      if (!survey) {
+        throw new NotFoundException(`Survey with ID ${id} not found`);
+      }
+      const questionIds = answers.map((answer) => answer.questionId);
+      const optionIds = answers.map((answer) => answer.selectedOptionId);
+
+      const questions = await manager.find(Question, {
+        where: { id: In(questionIds) },
+      });
+      const options = await manager.find(Option, {
+        where: { id: In(optionIds) },
+      });
+
+      for (const answer of answers) {
+        const { questionId, selectedOptionId } = answer;
+        const question = questions.find((q) => q.id === questionId);
+        const selectedOption = options.find((o) => o.id === selectedOptionId);
+
+        if (!question || !selectedOption) {
+          throw new NotFoundException(`Question or Option not found`);
+        }
+
+        const answerEntity = manager.create(Answer, {
+          question,
+          selectedOption,
+        });
+
+        await manager.save(answerEntity);
+      }
+
+      survey.isCompleted = true;
+
+      await this.surveyRepository.save(survey);
+      isSuccess = true;
+    });
+    this.logger.log(`Completed survey with ID ${id}`);
+    return isSuccess;
   }
 }
