bae83104ced507741c378af15a30ab527bacdad4
minwoo <minwoo9621@gmail.com>
2023-11-30 12:37:48 +0900

    Add forwarRef in survey , question module for solving circular ref

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,12 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionResolver } from './question.resolver';
 import { QuestionRepository } from './question.repository';
 import { DatabaseModule } from '../database/database.module';
 import { SurveyModule } from '../survey/survey.module';
-
 @Module({
-  imports: [DatabaseModule, SurveyModule],
+  imports: [DatabaseModule, forwardRef(() => SurveyModule)],
   providers: [QuestionService, QuestionResolver, ...QuestionRepository],
   exports: [QuestionService, ...QuestionRepository],
 })
--- src/survey/survey.module.ts
+++ src/survey/survey.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SurveyService } from './survey.service';
 import { SurveyResolver } from './survey.resolver';
 import { SurveyRepository } from './survey.repository';
 import { DatabaseModule } from '../database/database.module';
-
+import { AnswerModule } from '../answer/answer.module';
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, forwardRef(() => AnswerModule)],
   providers: [SurveyService, SurveyResolver, ...SurveyRepository],
   exports: [SurveyService, ...SurveyRepository],
 })
